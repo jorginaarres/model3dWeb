bbb3a310f455320cf9ad7351cb41a96f76de1ea3
juan.salmeron <jnssterrass@gmail.com>
2020-11-28 22:12:55 +0100

    Add Travis CI config and deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while getopts username:password: flag
+do
+    case "${flag}" in
+        username) username=${OPTARG};;
+        password) password=${OPTARG};;
+    esac
+done
+echo "Username: $username";
+echo "Age: $password";
