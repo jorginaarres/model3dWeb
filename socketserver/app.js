14c911ddab304f2edce35bb9496f6d6ef329e96e
juan.salmeron <jnssterrass@gmail.com>
2020-11-29 00:30:02 +0100

    Remove webserver endpoint

--- socketserver/app.js
+++ socketserver/app.js
@@ -33,10 +33,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function (socket) {
   socket.on('cat_configuration', function (msg) {
     io.emit('cat_configuration', msg);
@@ -44,5 +40,5 @@ io.on('connection', function (socket) {
 });
 
 http.listen(port, host, function () {
-  console.log('listening on *:' + port);
+  console.log('listening on ' + host + ':' + port);
 });
