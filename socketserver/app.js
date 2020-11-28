ccf88ffc9cb08d02f634b0702ebb3c24c97cd9f4
juan.salmeron <jnssterrass@gmail.com>
2020-11-28 23:00:42 +0100

    Remove corsoptions

--- socketserver/app.js
+++ socketserver/app.js
@@ -9,12 +9,8 @@ var cors = require('cors')
 var port = process.env.PORT || 3000;
 var host = 'hackeps.salmeronmoya.com';
 
-var corsOptions = {
-  origin: 'http://hackeps.salmeronmoya.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
