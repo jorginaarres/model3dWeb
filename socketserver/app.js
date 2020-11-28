a2bec9c8a92a198286eb636a73652d07302fb081
juan.salmeron <jnssterrass@gmail.com>
2020-11-29 00:13:30 +0100

    Update CORS whitelist

--- socketserver/app.js
+++ socketserver/app.js
@@ -1,22 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var host = 'hackeps.salmeronmoya.com';
+const cors_whitelist = [
+      'http://hackeps.salmeronmoya.com:80',
+      'http://hackeps.salmeronmoya.com:3000',
+      'http://localhost:80',
+      'http://localhost:3000',
+      'http://localhost:4200', '*'
+    ];
 const io = require("socket.io")(http, {
   cors: {
-    origin: ['http://hackeps.salmeronmoya.com', 'http://localhost:4200', '*'],
+    origin: cors_whitelist,
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
   }
 });
 var cors = require('cors')
-var port = process.env.PORT || 3000;
 
-const whitelist = ['http://hackeps.salmeronmoya.com', 'http://localhost', 'http://localhost:3000', '*'];
+var port = process.env.PORT || 3000;
 const corsOptions = {
   credentials: true, // This is important.
   origin: (origin, callback) => {
-    if (whitelist.includes(origin))
+    if (cors_whitelist.includes(origin))
       return callback(null, true)
 
     callback(new Error('Not allowed by CORS'));
