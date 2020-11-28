e3e906a43d59177bab78836638f133b9aa906a39
juan.salmeron <jnssterrass@gmail.com>
2020-11-28 22:30:41 +0100

    Enable CORS

--- socketserver/app.js
+++ socketserver/app.js
@@ -3,6 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
